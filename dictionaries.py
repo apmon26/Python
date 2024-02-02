b1b8013e13ba0c79c2b0e67d01f00b3d201874cc
Ashley Phan <avvp2612@gmail.com>
2024-02-02 14:34:47 -0500

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+# cat = {"name": "Ally", "age": 2, "is_cute": True}
+
+# print(cat)
+
+#Another way to create a dictionary with dict function
+#cat2 = dict(name = "Ally")
+# print(cat2)
+
+
+#Accessing data 
+# cat = {"name": "Ally", "age": 2, "is_cute": True}
+# print(cat["name"])
+
+# artist = {
+#     "first": "Neil",
+#     "last": "Young",
+# }
+
+# full_name = artist["first"] + " "+ artist["last"]
+
+#Iterating 
+
+cat = {"name": "Ally", "age": 2, "is_cute": True, "fur_color": "black", "weight": 6}
+
+for i in cat.keys():
+    print(i)
