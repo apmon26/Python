506e5b42221705fafa86706e57e7fcbb55557e57
Ashley Phan <avvp2612@gmail.com>
2024-04-14 23:07:52 -0400

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,22 @@
+# s = set([1,2,3,4,5,6,7,8,9,10,11,12])
+
+# s.add("apple")
+# s.remove("apple")
+# print(s)
+
+#two sets of students
+#set mathematical
+math_students = {"Anna", "Alice", "Bob", "Minh", "Kim", "John"}
+science_students = {"Jannet", "John", "Ken","Justin", "Adam", "Kim"}
+
+union = math_students | science_students
+print(union)
+
+intersection = math_students & science_students
+print(intersection)
+
+a = {x** 2 for x in range(10)}
+print(a)
+
+b = {char.upper() for char in "hello"}
+print(b)
