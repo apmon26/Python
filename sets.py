a245996f50f2e75b52a754c8d1be7534afab0cdb
Ashley Phan <avvp2612@gmail.com>
2024-04-16 20:45:50 -0400

    Update sets.py

--- sets.py
+++ sets.py
@@ -6,17 +6,20 @@
 
 #two sets of students
 #set mathematical
-math_students = {"Anna", "Alice", "Bob", "Minh", "Kim", "John"}
-science_students = {"Jannet", "John", "Ken","Justin", "Adam", "Kim"}
+# math_students = {"Anna", "Alice", "Bob", "Minh", "Kim", "John"}
+# science_students = {"Jannet", "John", "Ken","Justin", "Adam", "Kim"}
 
-union = math_students | science_students
-print(union)
+# union = math_students | science_students
+# print(union)
 
-intersection = math_students & science_students
-print(intersection)
+# intersection = math_students & science_students
+# print(intersection)
 
-a = {x** 2 for x in range(10)}
+# a = {x** 2 for x in range(10)}
+# print(a)
+
+# b = {char.upper() for char in "hello"}
+# print(b)
+import numpy as np
+a = np.random.randint(0, 10, 5)
 print(a)
-
-b = {char.upper() for char in "hello"}
-print(b)
